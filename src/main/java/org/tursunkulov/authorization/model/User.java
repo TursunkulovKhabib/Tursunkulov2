fba9bb5a65ed1d95ebe2ae5f15c0d551f514e712
Khabib <110659455+saitbuilder@users.noreply.github.com>
2025-03-06 23:41:51 +0300

    Доделка 3

--- src/main/java/org/tursunkulov/authorization/model/User.java
+++ src/main/java/org/tursunkulov/authorization/model/User.java
@@ -3,10 +3,12 @@ package org.tursunkulov.authorization.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @Getter
+@Setter
 public class User {
     private int id;
     private String username;
